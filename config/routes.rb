89f6f7df41f71eeab7054487a70d5c52ffcb36f1
gbkane <gbkane@gmail.com>
2015-05-19 22:02:16 +0000

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
+ 
 
   root 'static_pages#home'
 
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
   
   get 'signup' => 'users#new'
+  
   resources :users
   
   # The priority is based upon order of creation: first created -> highest priority.
