75e5493f6d66315f964b924254d6e5c3978f313e
gbkane <gbkane@gmail.com>
2015-05-21 03:55:01 +0000

    Add password resets & email configuration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,11 +16,11 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
-  def create
+    def create
     @user = User.new(user_params)
     if @user.save
       @user.send_activation_email
-      flash[:success] = "Please check your email to activate your account."
+      flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
     else
       render 'new'
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,33 +1,32 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  
+
   def setup
     ActionMailer::Base.deliveries.clear
   end
-  
+
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, user: { name: "",
-                                email: "user@invalid",
-                                password: "foo",
-                                password_confirmation: "bar" }
+      post users_path, user: { name:  "",
+                               email: "user@invalid",
+                               password:              "foo",
+                               password_confirmation: "bar" }
     end
     assert_template 'users/new'
-   # assert_select 'div#error_explanation'
+    assert_select 'div#error_explanation'
     assert_select 'div.field_with_errors'
   end
-  
+
   test "valid signup information with account activation" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post users_path, user: { name:    "Example User",
-                                email:  "user@example.com",
-                                password:               "password",
-                                password_confirmation:  "password" }
+      post users_path, user: { name:  "Example User",
+                               email: "user@example.com",
+                               password:              "password",
+                               password_confirmation: "password" }
     end
-    
     assert_equal 1, ActionMailer::Base.deliveries.size
     user = assigns(:user)
     assert_not user.activated?
@@ -43,7 +42,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     # Valid activation token
     get edit_account_activation_path(user.activation_token, email: user.email)
     assert user.reload.activated?
-    follow_redirect!    
+    follow_redirect!
     assert_template 'users/show'
     assert is_logged_in?
   end
